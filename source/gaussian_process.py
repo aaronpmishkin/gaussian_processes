Fix computation of log marginal likelhood, gradient of log marginal likelihood with respect to hyperparameters.

source/gaussian_process.py
@@ -2,13 +2,14 @@
 # @Author: aaronpmishkin
 # @Date:   2017-07-28 16:07:12
 # @Last Modified by:   aaronpmishkin
-# @Last Modified time: 2017-07-31 19:56:21
+# @Last Modified time: 2017-08-01 16:49:48
 
 # Implementation adapted from Gaussian Processes for Machine Learning; Rasmussen and Williams, 2006
 
 import numpy as np
-from scipy.linalg import cholesky, inv, det
+from scipy.linalg import cholesky, inv, solve_triangular
 from scipy.stats import norm
+from scipy.optimize import minimize
 import matplotlib.pyplot as plt
 
 import mean_functions
@@ -32,19 +33,30 @@ class GaussianProcess():
             This expresses the covariance between inputs.
         mean_function:
             The prior mean function for the gaussian process.
-        sigma: number
+        obs_variance: number
             The variance of the gaussian noise in the targets.
     """
 
-    def __init__(self, X, Y, kernel, mean_function=mean_functions.zero_mean, sigma=1):
+    def __init__(self, X, Y, kernel, mean_function=mean_functions.zero_mean, obs_variance=1):
+        self.__plot_density__ = 200
+        self.__plot_delta__ = 0.2
+
         self.X = X
         self.Y = Y
         self.kernel = kernel
         self.mean_function = mean_function
         self.mu = mean_function(X)
-        self.sigma = sigma
+        self.obs_variance = obs_variance
 
-        self.K = kernel.cov(X) + ((sigma ** 2) * np.identity(X.shape[0]))
+        self.K = kernel.cov(X) + (obs_variance * np.identity(X.shape[0]))
+
+    def get_hyperparameters(self):
+        return np.append(np.array([self.obs_variance]), self.kernel.get_hyperparameters())
+
+    def set_hyperparameters(self, theta):
+        self.obs_variance = theta[0]
+        self.kernel.set_hyperparameters(theta[1:])
+        self.K = self.kernel.cov(self.X) + (self.obs_variance * np.identity(self.X.shape[0]))
 
     def predict(self, X_star, noise=True):
         """ predict
@@ -57,18 +69,17 @@ class GaussianProcess():
                 Whether or not to include noise estimate in predictions.
         """
         K_star = self.kernel.cov(X_star, self.X)
-
         L = cholesky(self.K, lower=True)
-        L_inv = inv(L)
 
-        alpha = np.dot(L_inv.T, np.dot(L_inv, (self.Y - self.mu)))
-
+        alpha = solve_triangular(L, (self.Y - self.mu), lower=True)
+        alpha = solve_triangular(L.T, alpha, lower=False)
         f_bar = self.mean_function(X_star) + np.dot(K_star, alpha)
 
-        cov = self.kernel.cov(X_star) - np.dot(np.dot(K_star, L_inv.T), np.dot(L_inv, K_star.T))
+        v = solve_triangular(L, K_star.T, lower=True)
+        cov = self.kernel.cov(X_star) - np.dot(v.T, v)
 
         if noise:
-            cov += (self.sigma ** 2) * np.identity(cov.shape[0])
+            cov += self.obs_variance * np.identity(cov.shape[0])
 
         return f_bar, cov  # Return the mean and covariance matrix for X_star
 
@@ -85,7 +96,7 @@ class GaussianProcess():
                 The lower and upper quantiles that define the desired confidence bounds.
         """
         f_bar, cov = self.predict(X_star, noise)
-        variance = np.diag(cov)
+        sd = np.sqrt(np.diag(cov))
         upper_quantiles = np.copy(f_bar)
         lower_quantiles = np.copy(f_bar)
 
@@ -93,7 +104,7 @@ class GaussianProcess():
 
         # Assuming that the likelihood is Gaussian:
         for i, mean in enumerate(f_bar):
-            quantiles = norm.interval(alpha, loc=mean, scale=variance[i])
+            quantiles = norm.interval(alpha, loc=mean, scale=sd[i])
             lower_quantiles[i] = quantiles[0]
             upper_quantiles[i] = quantiles[1]
 
@@ -106,57 +117,94 @@ class GaussianProcess():
         I_matrix = np.identity(self.X.shape[0])
 
         if theta is None:
-            theta = np.array([self.sigma, self.kernel.length_scale, self.kernel.sigma])
+            theta = np.array([self.obs_variance, self.kernel.length_scale, self.kernel.var])
             K = self.K
         else:
-            K = self.kernel.cov(self.X, theta=theta[1:]) + ((theta[0] ** 2) * I_matrix)
+            K = self.kernel.cov(self.X, theta=theta[1:]) + (theta[0] * I_matrix)
 
-        L = cholesky(K, lower=True)
-        L_inv = inv(L)
-        K_inv = np.dot(L_inv.T, L_inv)
         Y_cent = self.Y - self.mu
+        L = cholesky(K, lower=True)
 
-        fit_term = np.log(np.dot(Y_cent.T, np.dot(K_inv, Y_cent)))
+        alpha = solve_triangular(L, Y_cent, lower=True)
+        alpha = solve_triangular(L.T, alpha, lower=False)
 
-        complexity_term = np.log(det(K))
+        fit_term = -0.5 * np.dot(Y_cent.T, alpha)
 
-        normalizing_term = self.X.shape[0] * np.log(2 * np.pi)
+        complexity_term = -np.log(np.diag(L)).sum()
 
-        return 0.5 * (-fit_term - complexity_term - normalizing_term)
+        normalizing_term = -0.5 * self.X.shape[0] * np.log(2 * np.pi)
 
-    def grad_log_likelihood(self, theta=None):
+        return (fit_term + complexity_term + normalizing_term).sum()
+
+    def log_likelihood_gradient(self, theta=None):
         # The first element of the hyperparameter vector is the observation variance.
 
         if theta is None:
-            theta = np.array([self.sigma, self.kernel.length_scale, self.kernel.sigma])
+            theta = np.array([self.obs_variance, self.kernel.length_scale, self.kernel.var])
 
         I_matrix = np.identity(self.X.shape[0])
         grad = np.copy(theta)
 
-        K = self.kernel.cov(self.X, theta=theta[1:]) + ((theta[0] ** 2) * I_matrix)
+        K = self.kernel.cov(self.X, theta=theta[1:]) + (theta[0] * I_matrix)
 
         L = cholesky(K, lower=True)
         L_inv = inv(L)
-        K_inv = np.dot(L_inv.T, L_inv)
-        alpha = np.dot(K_inv, (self.Y - self.mu))
+        alpha = solve_triangular(L, (self.Y - self.mu), lower=True)
+        alpha = solve_triangular(L.T, alpha, lower=False)
+        beta = np.dot(alpha, alpha.T) - np.dot(L_inv.T, L_inv)
 
-        beta = np.dot(alpha, alpha.T) - K_inv
+        grad[0] = 0.5 * np.trace(beta)
 
-        grad[0] = 0.5 * np.trace(np.dot(beta, 2 * theta[0] * I_matrix))
+        dK_dtheta = self.kernel.cov_gradient(self.X, theta=theta[1:])
 
-        dkernel_params = self.kernel.grad_cov(self.X, theta=theta[1:])
-
-        for i, d_cov in enumerate(dkernel_params):
-            grad[i + 1] = 0.5 * np.trace(np.dot(beta, d_cov))
+        for i, dK_dh in enumerate(dK_dtheta):
+            grad[i + 1] = 0.5 * np.trace(np.dot(beta, dK_dh))
 
         return grad
 
+    def __objective__(self, theta=None):
+        return -1 * self.log_likelihood(theta)
+
+    def __objective_grad__(self, theta=None):
+        return -1 * self.log_likelihood_gradient(theta)
+
+    def optimize(self, n_restarts=10):
+        best_theta = None
+        min_objective = np.inf
+
+        for start in np.random.uniform(np.array([0, 0, 0]),
+                                       np.array([10, 10, 10]),
+                                       size=(n_restarts, 3)):
+
+            res = minimize(self.__objective__,
+                           x0=start,
+                           method="L-BFGS-B",
+                           jac=self.__objective_grad__,
+                           bounds=np.array([(1e-8, None), (1e-8, None), (0, None)])
+                           )
+
+            if res.fun < min_objective:
+                min_objective = res.fun
+                best_theta = res.x
+
+        return best_theta, -1 * min_objective
+
     def plot(self, show_data=True):
-        mean_x = np.linspace(-3, 3, 100).reshape(100, 1)
+        if self.X.shape[1] != 1:
+            raise ValueError('Cannot plot with multi-dimensional inputs')
+
+        minVal = np.min(self.X)
+        maxVal = np.max(self.X)
+        delta = (maxVal - minVal) * self.__plot_delta__
+
+        mean_x = np.linspace(np.floor(minVal - delta),
+                             np.ceil(maxVal + delta),
+                             self.__plot_density__).reshape(self.__plot_density__, 1)
+
         mean_y, cov = self.predict(mean_x)
         lq, uq = self.predict_quantiles(mean_x, noise=True)
 
-        mean_x = mean_x.reshape(100)
+        mean_x = mean_x.reshape(self.__plot_density__)
 
         fig = plt.figure()
         plt.plot(mean_x, mean_y, 'k', label='Predicted Mean')
@@ -171,3 +219,4 @@ class GaussianProcess():
         plt.legend()
 
         return fig
+

source/gp_test.py
@@ -2,13 +2,14 @@
 # @Author: aaronpmishkin
 # @Date:   2017-07-28 21:31:56
 # @Last Modified by:   aaronpmishkin
-# @Last Modified time: 2017-07-31 19:58:00
+# @Last Modified time: 2017-08-01 16:49:28
 
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.gaussian_process import GaussianProcessRegressor
 import gaussian_process
 import kernels
 import mean_functions
@@ -20,23 +21,39 @@ Y = np.sin(X) + np.random.normal(loc=0, scale=0.1, size=X.shape)
 sin_x = np.linspace(-3, 3, 100)
 sin_y = np.sin(sin_x)
 
-# Plot the ground truth:
-plt.figure()
-plt.plot(X, Y, 'x', sin_x, sin_y)
-plt.legend(['Sampled Data', 'Ground Truth'])
-
-rbf_kernel = kernels.RBF(length_scale=1, sigma=1)
+rbf_kernel = kernels.RBF(length_scale=1, var=1)
 
 
-gp = gaussian_process.GaussianProcess(X, Y, rbf_kernel, mean_functions.zero_mean, sigma=0.1)
+gp = gaussian_process.GaussianProcess(X, Y, rbf_kernel, mean_functions.zero_mean, obs_variance=0.1)
 
 
 likelihood = gp.log_likelihood(np.array([0.1, 1, 1]))
-grad_likelihood = gp.grad_log_likelihood(np.array([0.1, 1, 1]))
+grad_likelihood = gp.log_likelihood_gradient(np.array([0.1, 1, 1]))
 
-plot = gp.plot()
 
-plt.plot(sin_x, sin_y, 'g', label='Ground Truth')
+gp.plot()
 
-plt.legend()
+
+print('My Likelihood: ', likelihood, grad_likelihood)
+
+import GPy
+
+k = GPy.kern.RBF(1, lengthscale=1)
+gp_GPy = GPy.models.GPRegression(X, Y, k, noise_var=0.1)
+print('GPy Likelihood: ', gp_GPy.log_likelihood(), gp_GPy.objective_function_gradients())
+
+gp_GPy.plot()
+
+theta, op_likelihood = gp.optimize()
+gp.set_hyperparameters(theta)
+print(theta, op_likelihood)
+
+gp_GPy.optimize()
+print(gp_GPy)
+
+gp.plot()
+gp_GPy.plot()
+
+X_star = np.random.uniform(low=-3, high=3, size=5).reshape(5, 1)
+
 plt.show()

source/kernels.py
@@ -2,7 +2,7 @@
 # @Author: aaronpmishkin
 # @Date:   2017-07-28 21:07:21
 # @Last Modified by:   aaronpmishkin
-# @Last Modified time: 2017-07-31 18:53:31
+# @Last Modified time: 2017-08-01 16:49:48
 
 import numpy as np
 from scipy.spatial.distance import cdist
@@ -17,13 +17,20 @@ class RBF():
     ----------
         length_scale: number
             The length scale of the kernel function.
-        sigma: number
+        var: number
             The variance magnitude of the kernel function.
     """
 
-    def __init__(self, length_scale=1, sigma=1):
+    def __init__(self, length_scale=1, var=1):
         self.length_scale = length_scale
-        self.sigma = sigma
+        self.var = var
+
+    def get_hyperparameters(self):
+        return np.array([self.length_scale, self.var])
+
+    def set_hyperparameters(self, theta):
+        self.length_scale = theta[0]
+        self.var = theta[1]
 
     def cov(self, X, Y=None, theta=None):
         """ cov
@@ -42,17 +49,17 @@ class RBF():
             Y = X
 
         if theta is None:
-            theta = np.array([self.length_scale, self.sigma])
+            theta = np.array([self.length_scale, self.var])
 
         # Compute a matrix of squared eucledian distances between X and Y
         dist = cdist(X, Y, 'sqeuclidean')
 
-        K = (theta[1] ** 2) * np.exp(dist / (-2 * (theta[0] ** 2)))
+        K = theta[1] * np.exp(dist / (-2 * (theta[0] ** 2)))
 
         return K
 
-    def grad_cov(self, X, theta=None):
-        """ grad_cov
+    def cov_gradient(self, X, theta=None):
+        """ cov_gradient
         Compute the gradient of the covariance matrix of X with respect to the hyperparameters
         of the RBF kernel.
         Arguments:
@@ -64,16 +71,16 @@ class RBF():
         """
 
         if theta is None:
-            theta = np.array([self.length_scale, self.sigma])
+            theta = np.array([self.length_scale, self.var])
 
         dist = cdist(X, X, 'sqeuclidean')
 
         K = np.exp(dist / (-2 * (theta[0] ** 2)))
 
-        dK_dsigma = 2 * theta[1] * K
+        dK_dl = theta[1] * (theta[0] ** -5) * dist * K
 
-        dK_dl = (theta[1] ** 2) * (theta[0] ** -5) * dist * K
+        dK_dvar = K
 
-        return np.array([dK_dl, dK_dsigma])
+        return np.array([dK_dl, dK_dvar])
 
 