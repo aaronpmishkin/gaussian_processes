Fix yet another bug in liklihood gradients.

source/gaussian_process.py
@@ -2,7 +2,7 @@
 # @Author: aaronpmishkin
 # @Date:   2017-07-28 16:07:12
 # @Last Modified by:   aaronpmishkin
-# @Last Modified time: 2017-08-08 11:36:47
+# @Last Modified time: 2017-08-08 19:31:21
 
 # Implementation adapted from Gaussian Processes for Machine Learning; Rasmussen and Williams, 2006
 
@@ -69,7 +69,7 @@ class GaussianProcess():
         """
         self.theta = theta
         self.kernel.set_parameters(theta[1:])
-        self.K = self.kernel.cov(self.X) + (theta[0] * np.identity(self.X.shape[0]))
+        _, self.K, self.L, self.alpha, _ = self.__covariance_at_theta__(theta)
 
     def predict(self, X_star, noise=True):
         """ predict
@@ -250,8 +250,8 @@ class GaussianProcess():
 
         if bounds is None:
             bounds = np.ones((self.kernel.num_parameters + 1, 2))
-            bounds[:, 0] = bounds[:, 0] * 1e-8
-            bounds[:, 1] = bounds[:, 1] * 100
+            bounds[:, 0] = bounds[:, 0] * 0.01
+            bounds[:, 1] = bounds[:, 1] * 1000
 
         if len(fixed_params) != 0:
             bounds = np.delete(bounds, fixed_params, axis=0)

source/gp_test.py
@@ -2,110 +2,46 @@
 # @Author: aaronpmishkin
 # @Date:   2017-07-28 21:31:56
 # @Last Modified by:   aaronpmishkin
-# @Last Modified time: 2017-08-08 10:37:52
+# @Last Modified time: 2017-08-08 19:55:36
 
 import numpy as np
+# from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 import GPy
 import gaussian_process
 import kernels
 import bayesian_optimization
-import parse_value_chart
-from mean_functions import AdditiveMean
 
+X = np.random.uniform(low=-5, high=5, size=(5, 1))
 
-# X = np.random.uniform(low=[0, 0], high=[10, 10], size=(5, 2))
+Y = -X
 
+print(X, Y)
 
-# Y = (X[:, 0] ** 2) + X[:, 1] + np.random.normal(loc=0, scale=4, size=(5,))
-# Y = Y.reshape(5,1)
+rbf_kernel = kernels.RBF(dim=1, length_scale=1., var=1.)
+gp = gaussian_process.GaussianProcess(X, Y, rbf_kernel, obs_variance=1.)
 
-# print(X, Y)
-# base_kernel_0 = kernels.RBF(dim=1, length_scale=1, var=1)
-# base_kernel_1 = kernels.RBF(dim=1, length_scale=1, var=1)
+gp.plot()
 
-# additive_kernel = kernels.Additive(dim=2, order=2, base_kernels=[base_kernel_0, base_kernel_1])
-# gp_additive = gaussian_process.GaussianProcess(X, Y, additive_kernel, obs_variance=1)
+likelihood = gp.log_likelihood(np.array([1., 10.68, 1.]))
+grad_likelihood = gp.log_likelihood_gradient(np.array([1., 10.68, 1.]))
+print('My Likelihood: ', likelihood, grad_likelihood)
 
-# likelihood = gp_additive.log_likelihood(np.array([1, 1, 1, 1, 1, 1, 1]))
-# grad_likelihood = gp_additive.log_likelihood_gradient(np.array([1, 1, 1, 1, 1, 1, 1]))
+# Double Check that the Likelihood and Gradients are correct:
+k = GPy.kern.RBF(1, lengthscale=10.68)
+gp_GPy = GPy.models.GPRegression(X, Y, k, noise_var=1)
+print('GPy Likelihood: ', gp_GPy.log_likelihood(), gp_GPy.objective_function_gradients())
 
-# print('GP Additive Likelihood:', likelihood, grad_likelihood)
+gp_GPy.plot()
 
-# rbf_kernel = kernels.RBF(dim=1, length_scale=1, var=1)
-# gp = gaussian_process.GaussianProcess(X, Y, rbf_kernel, obs_variance=1)
-
-# likelihood = gp.log_likelihood(np.array([1, 1, 1]))
-# grad_likelihood = gp.log_likelihood_gradient(np.array([0.1, 1, 1]))
-
-
-# print('My Likelihood: ', likelihood, grad_likelihood)
-
-
-# k = GPy.kern.RBF(2, lengthscale=1)
-# gp_GPy = GPy.models.GPRegression(X, Y, k, noise_var=1)
-# print('GPy Likelihood: ', gp_GPy.log_likelihood(), gp_GPy.objective_function_gradients())
-
-
-# theta, op_likelihood = gp_additive.optimize()
-# print('Optimized Additive Model: ', theta, op_likelihood)
-# gp_additive.set_hyperparameters(theta)
-
-
-# theta, op_likelihood = gp.optimize()
-# gp.set_hyperparameters(theta)
-# print('Optimized RBF Model: ', theta, op_likelihood)
-
-# gp_GPy.optimize()
-# print(gp_GPy)
-
-
-with open('../data/ValueCharts/HotelChart.json', 'r') as TestChart:
-            value_chart = TestChart.read().replace('\n', '')
-
-(value_chart,
- features,
- objective_map,
- bounds,
- X,
- utility_functions) = parse_value_chart.parse_valuechart(value_chart)
-
-print(X)
-Y = np.array([27, 75, 42, 53, 86, 61]).reshape(X.shape[0], 1)
-additive_kernel = kernels.Additive(dim=5, order=1, base_kernels=[kernels.RBF(dim=1),
-                                                                 kernels.RBF(dim=1),
-                                                                 kernels.RBF(dim=1),
-                                                                 kernels.RBF(dim=1),
-                                                                 kernels.RBF(dim=1)])
-additive_mean = AdditiveMean(dim=2, mean_functions=utility_functions)
-gp = gaussian_process.GaussianProcess(X, Y, additive_kernel, mean_function=additive_mean)
 theta, op_likelihood = gp.optimize()
 gp.set_hyperparameters(theta)
+print('Optimized RBF Model: ', theta, op_likelihood)
 
-bounds = np.array(bounds)
+gp.plot()
 
-fixed_features = []
-fixed_values = []
-for i, feature in enumerate(features):
-    if feature['type'] == 'discrete':
-        fixed_features.append(i)
-        fixed_values.append(0)
+gp_GPy.optimize()
+print(gp_GPy)
+gp_GPy.plot()
+plt.show()
 
-fixed_features = np.array(fixed_features)
-fixed_values = np.array(fixed_values)
-
-x = bayesian_optimization.choose_sample(bayesian_optimization.upper_confidence_bound,
-                                        gp,
-                                        bounds,
-                                        fixed_features,
-                                        fixed_values,
-                                        10,
-                                        True)
-print(x)
-
-x = bayesian_optimization.choose_categories(
-    x,
-    bayesian_optimization.upper_confidence_bound,
-    gp, objective_map)
-
-print(x)

source/kernels.py
@@ -2,7 +2,7 @@
 # @Author: aaronpmishkin
 # @Date:   2017-07-28 21:07:21
 # @Last Modified by:   aaronpmishkin
-# @Last Modified time: 2017-08-04 15:46:11
+# @Last Modified time: 2017-08-08 19:51:21
 
 import numpy as np
 from scipy.spatial.distance import cdist
@@ -90,7 +90,7 @@ class RBF():
 
         K = np.exp(dist / (-2 * (theta[0] ** 2)))
 
-        dK_dl = theta[1] * (theta[0] ** -5) * dist * K
+        dK_dl = theta[1] * (theta[0] ** -3) * dist * K
 
         dK_dvar = K
 